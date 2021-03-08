ef0aae2443116b784e4011733c310d5fcb0b7ede
tecking <tets.morikawa@gmail.com>
2021-03-08 23:29:48 +0900

    add configuration file

---
+++ Config/setting.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * [Enable reCAPTCHA v3] reCAPTCHA v3 拡張 設定ファイル
+ *
+ * @copyright  Copyright 2021 - , tecking
+ * @link       https://github.com/tecking
+ * @package    tecking.bcplugins.enable_re_captcha
+ * @since      baserCMS v 4.3.7.1
+ * @version    0.6.0
+ * @license    MIT License
+ */
+$config = [
+		'EnableReCaptcha' => [
+			'threshold' => 0.5 // スパム bot と判定するスコアのしきい値
+		]
+	];
